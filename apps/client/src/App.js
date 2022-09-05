54c583ff4c2c6eaa181c120dc4e5c181470b00b9
azizbenyaala98 <azizbenyaala@gmail.com>
2022-09-05 18:13:24 +0200

    form integration client side

--- apps/client/src/App.js
+++ apps/client/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
-
+import {Typography,Card,CardContent,Grid,TextField,Button,IconButton,FormControl,InputLabel,InputAdornment,OutlinedInput} from '@mui/material'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' >
+      <Typography gutterBottom variant="h3" align="center">
+        React-app
+      </Typography>
+    <Card>
+      <CardContent>
+        <Grid container spacing={1}>
+          <Grid xs={12} sm={6} item>
+            <TextField label="title" placeholder='enter product tiltle' variant="filled" fullWidth required/>
+            </Grid>
+            <Grid xs={12} sm={6} item>
+            <TextField label="id" placeholder='enter product id' variant="filled" fullWidth required/>
+           
+            </Grid>
+            <Grid xs={12} sm={6} item>
+            <TextField label="description" multiline rows={4} placeholder='enter product description' variant="filled" fullWidth required/>
+           
+            </Grid>
+            <Grid xs={12} sm={6}>
+            <Button variant="contained" component="label"> Upload product picture  <input hidden accept="image/*" multiple type="file" /></Button>
+            <IconButton color="primary" aria-label="upload picture" component="label">
+            </IconButton>
+            </Grid>
+
+            
+            <Grid xs={12} sm={6}>
+            <FormControl fullWidth variant="filled">
+          <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-amount"
+           
+            startAdornment={<InputAdornment position="start">$</InputAdornment>}
+            label="Amount"
+          />
+        </FormControl>
+        </Grid>
+            
+            </Grid>
+      </CardContent>
+    </Card>
+      
+        
     </div>
   );
 }
